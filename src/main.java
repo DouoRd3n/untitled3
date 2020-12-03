4b4273d15064742cc83d090dbcf9b23bc025646b
Douord3n <DouOrd3n@gmail.com>
2020-12-03 19:59:17 +0200

    task5

---
+++ src/main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+// 1 заповнити довільними значеннями. getList 50шт
+// 2 вивести на екран
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = getList(50);
+
+      printListOneRow(list);
+
+
+
+        printListOneRow(list);
+    }
+    public static LinkedList<Integer> getList(int n){
+        LinkedList<Integer> list1 = new LinkedList<>();
+        for (int i = 0; i < n ; i++) {
+            list1.add((int) (Math.random()*100+1));
+
+        } return list1;
+    }
+
+    public static void printListOneRow(LinkedList<Integer> list){
+        for (int i = 0; i <list.size() ; i++) {
+
+                System.out.print(list.get(i) + " ");
+            }
+
+
+    }
+}
